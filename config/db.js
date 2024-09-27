c032522d27952ef3a54f4afe88c4de370de946d8
Anujameya <anuj.patel@iameya.in>
2024-09-27 23:26:32 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/seatBooking');
+    await mongoose.connect("mongodb+srv://Mohan5055:imgrMBme3W4mVURh@cluster0.b3s6d.mongodb.net/SeatBooking?retryWrites=true&w=majority&appName=Cluster0");
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed', error);
@@ -11,3 +11,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+//imgrMBme3W4mVURh
